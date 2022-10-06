da105d5f6873a24b6320512fb7970a6a23b9c35e
tomimandalap <tomi.mandala@privyid.tech>
2022-10-06 12:31:20 +0700

    feat: add utils and menu navbar

---
+++ src/utils/menu.js
@@ -0,0 +1,24 @@
+export default {
+  data() {
+    return {
+      listMenu: [
+        {
+          name: 'Home',
+          path: '/web',
+        },
+        {
+          name: 'Categories',
+          path: '/web/categories',
+        },
+        {
+          name: 'Pricing',
+          path: '/web/pricing',
+        },
+        {
+          name: 'Study Case',
+          path: '/web/study-case',
+        },
+      ],
+    }
+  },
+}
