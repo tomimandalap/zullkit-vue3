ebcf7e201b46f64a539394e6f3e7e0da2682943c
tomimandalap <tomi.mandala@privyid.tech>
2022-10-06 20:32:05 +0700

    feat: add animation and style in page categories, pricing and study case

--- src/views/categories.vue
+++ src/views/categories.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Categories</h1>
+  <div
+    data-aos="fade-up"
+    class="container flex flex-col items-center justify-center min-h-screen"
+  >
+    <h1 class="text-3xl mb-10 text-purple-700 font-bold">Comming Soon!</h1>
+    <img src="@/assets/img/feature.svg" alt="feature.svg" width="350" />
+  </div>
 </template>
