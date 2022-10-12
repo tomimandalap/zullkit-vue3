db1189955aecdfd92d5f649bf0d986c2ae45a872
tomimandalap <tomi.mandala@privyid.tech>
2022-10-12 19:14:44 +0700

    fix: proxy

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,28 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  let data = {}
   const env = loadEnv(mode, process.cwd())
-  if (['serve'].includes(command)) {
-    data = {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url)),
+  const data = {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      proxy: {
+        '/api-web': {
+          target: env.VITE_API_ENDPOINT,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api-web/, ''),
         },
       },
-      server: {
-        proxy: {
-          '/api-web': {
-            target: env.VITE_API_ENDPOINT,
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api-web/, ''),
-          },
-        },
-      },
-    }
+    },
   }
-  return data
+
+  if (['serve'].includes(command)) {
+    return data
+  } else {
+    return data
+  }
 })
