4951beb7af31955c22fe9b2af59cc5ca2f283af6
tomimandalap <tomi.mandala@privyid.tech>
2022-10-12 19:30:48 +0700

    fix: proxy

--- vite.config.js
+++ vite.config.js
@@ -24,11 +24,16 @@ export default ({ mode }) => {
     },
     server: {
       proxy: {
-        '/api-web/.*': {
-          target: env.VITE_API_ENDPOINT,
+        '/api-web/': {
+          target: env?.VITE_API_ENDPOINT,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api-web/, ''),
         },
+      },
+      // Proxying websockets or socket.io
+      '/socket.io': {
+        target: 'ws://localhost:3000',
+        ws: true,
       },
     },
   })
