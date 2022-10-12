e0173b0671fc5e66e28162ff463048a32b74c921
tomimandalap <tomi.mandala@privyid.tech>
2022-10-12 19:58:08 +0700

    fix: add proxy

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,9 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url)),
-//     },
-//   },
-// })
-
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return defineConfig({
+export default defineConfig(async ({ command, mode }) => {
+  const env = await loadEnv(mode, process.cwd(), '')
+  return {
     plugins: [vue()],
     resolve: {
       alias: {
@@ -24,13 +15,13 @@ export default ({ mode }) => {
     },
     server: {
       proxy: {
-        // '/api-web': `${env.VITE_API_ENDPOINT}`,
         '^/api-web/.*': {
           target: env.VITE_API_ENDPOINT,
           changeOrigin: true,
+          ws: true,
           rewrite: (path) => path.replace(/^\/api-web/, ''),
         },
       },
     },
-  })
-}
+  }
+})
